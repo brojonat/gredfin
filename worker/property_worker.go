6ebb934b3c169c7a6e717d1577ca38d8cf07e475
Jon Brown <brojonat@gmail.com>
2024-05-20 21:09:14 -0700

    reorg

--- worker/property_worker.go
+++ worker/property_worker.go
@@ -2,12 +2,16 @@ package worker
 
 import (
 	"bytes"
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/brojonat/gredfin/server/dbgen"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 func claimProperty(endpoint string, headers http.Header) (*dbgen.Property, error) {
@@ -64,4 +68,51 @@ func createProperty(endpoint string, h http.Header, c *dbgen.CreatePropertyParam
 		return fmt.Errorf(res.Status)
 	}
 	return nil
+}
+
+func uploadSearchResults(endpoint string, h http.Header, l *slog.Logger, b []byte) {
+	// get the CSV of properties from a search search and dump it into a csv
+	file, err := os.CreateTemp("", "search")
+	if err != nil {
+		l.Error("error opening temp file", "error", err.Error())
+		return
+	}
+	defer os.Remove(file.Name())
+	file.Write(b)
+
+	// read the csv
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = -1 // Allow variable number of fields
+	data, err := reader.ReadAll()
+	if err != nil {
+		l.Error("error reading CSV", "error", err.Error())
+		return
+	}
+
+	// upload each property
+	for _, row := range data {
+		// the row only has an address, no property/listing ids, so we need to
+		// run a search NOW to get that info. The good thing is we're working fro
+		// a property so we can go after the exact match. FIXME: there's still some
+		// work to implement here.
+		propertyID := "123"
+		listingID := "456"
+		addr := row[5]
+		cpp := &dbgen.CreatePropertyParams{
+			PropertyID: propertyID,
+			ListingID:  listingID,
+			Address:    pgtype.Text{String: addr},
+		}
+		err := createProperty(endpoint, h, cpp)
+		if err != nil {
+			l.Error(
+				"error creating property",
+				"error", err.Error(),
+				"propertyID", propertyID,
+				"listingID", listingID,
+				"address", addr,
+			)
+			continue
+		}
+	}
 }
--- worker/search_worker.go
+++ worker/search_worker.go
@@ -2,13 +2,10 @@ package worker
 
 import (
 	"bytes"
-	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io"
-	"log/slog"
 	"net/http"
-	"os"
 
 	"github.com/brojonat/gredfin/server/dbgen"
 	"github.com/jackc/pgx/v5/pgtype"
@@ -44,40 +41,7 @@ func claimSearch(endpoint string, h http.Header) (*dbgen.Search, error) {
 	return &s, nil
 }
 
-func uploadSearchResults(endpoint string, h http.Header, l *slog.Logger, b []byte) {
-	// get the CSV of properties for this search and dump it into a csv
-	file, err := os.CreateTemp("", "search")
-	if err != nil {
-		l.Error("error opening temp file", "error", err.Error())
-		return
-	}
-	defer os.Remove(file.Name())
-	file.Write(b)
-
-	// read the csv
-	reader := csv.NewReader(file)
-	reader.FieldsPerRecord = -1 // Allow variable number of fields
-	data, err := reader.ReadAll()
-	if err != nil {
-		l.Error("error reading CSV", "error", err.Error())
-		return
-	}
-
-	// upload each property
-	for _, row := range data {
-		addr := fmt.Sprintf("%s, %s, %s, %s", row[3], row[4], row[5], row[6])
-		err := createSearch(endpoint, h, pgtype.Text{String: addr})
-		if err != nil {
-			l.Error(
-				"error creating property",
-				"error", err.Error(),
-				"address", addr,
-			)
-			continue
-		}
-	}
-}
-
+// FIXME: move this into a cli or something
 func createSearch(endpoint string, h http.Header, addr pgtype.Text) error {
 	b, err := json.Marshal(addr)
 	if err != nil {
